9f3570f82793538d790b2acf5cf15612c75a5aae
0tanh <betty.lossless@gmail.com>
2026-06-21 20:13:02 +0200

    feat: more explicit types

--- src/lib/PreferenceTypes.ts
+++ src/lib/PreferenceTypes.ts
@@ -2,11 +2,11 @@
  * Non-type erased enum of different prefences!
  */
 export const PreferenceIcon = {
-  OFF_LIMIT: "OFF_LIMIT", 
-  PREFER_NOT: "PREFER_NOT", 
-  MAYBE: "MAYBE", 
-  LIKE: "LIKE", 
-  MUST:"MUST", 
+  OFF_LIMIT: 1, 
+  PREFER_NOT: 2, 
+  MAYBE: 3, 
+  PREFER: 4, 
+  MUST:5, 
   //for for loops
   *[Symbol.iterator]() {
     for (const value of Object.keys(this)) {
@@ -15,6 +15,9 @@ export const PreferenceIcon = {
   }
 } as const
 
+/**
+ * A structured type version of the preference Icon Constant
+ */
 export type PreferenceIconType= typeof PreferenceIcon[keyof typeof PreferenceIcon];
 
 
@@ -65,4 +68,63 @@ export interface PreferenceContext {
   currentPrefIcon: PreferenceIconType | null;
   prefSize: number;
   currentPreferenceProfile: PreferenceProfile
+}
+
+
+/**
+ * Non-type erased enum different configurations of showing notes
+ */
+export const NotesConfig= {
+  ALL_NOTES: "ALL_NOTES", //show all notes that appear regardless of match
+  HIDE_PERFECT: "HIDE_PERFECT", //show all_notes except notes attached to perfect matches
+  ONLY_MISALIGNED: "ONLY_MISALIGNED", //show only notes on misaligned preferences
+  //for for loops
+  *[Symbol.iterator]() {
+    for (const value of Object.keys(this)) {
+      yield value;
+    }
+  }
+} as const
+
+/**
+ * A structured type of all the notes to be shown
+ */
+export type NotesConfigType= typeof NotesConfig [keyof typeof NotesConfig];
+
+/**
+ * This interface demonstrates the current configuration
+ * between any number of PreferenceProfiles
+ */
+export interface ComparisonConfig {
+  max_acceptable_misalign : number //maximum number of icon points off that two icons are allowed to be
+  show_full : boolean
+  notes_config : NotesConfigType 
+  show_perfect_matches : boolean //show preferences that have perfectly matched icons
+  show_pain_points: boolean //show preferences that are diametrically opposed
+  show_uncomparable: boolean //show all preferences that could not be compared because they did not appear in all profiles
+}
+
+export interface MetamorIconMap {
+  metamorName: string
+  icon: PreferenceIconType
+}
+
+/**
+ * This interface demonstrates the shape of a single preference being compared
+ * 
+ */
+export interface ComparisonUnit {
+  prefName: string
+  metamorIconMap: MetamorIconMap[]
+}
+
+/**
+ * This interface encapsulates the current comparison context
+ */
+export interface ComparisonContext {
+  allComparisonUnits: ComparisonUnit[]
+  all_uncomparable_prefs?:{
+    metamorName: string,
+    unmatchedPref: Preference
+  }[]
 }
